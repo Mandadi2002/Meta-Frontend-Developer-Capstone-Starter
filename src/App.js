42a70fc0e7c66aa1ffb9efd0d0bb0e71a7624e8e
Mandadi2002 <https://github.com/Mandadi2002>
2025-01-11 21:06:40 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './App.css';
 import Nav from './components/Nav';
+import Header from './components/Header';
 
 function App() {
-  return (
-    <>
-    <Nav/>
-    </>
-  );
+    return (
+        <>
+        <Nav/>
+        <Header/>
+        </>
+
+    )
 }
 
 export default App;
+
+
