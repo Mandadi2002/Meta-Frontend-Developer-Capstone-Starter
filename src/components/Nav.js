6204d33bc43fd860dfb41b5c1c2c43c618715c62
Mandadi2002 <https://github.com/Mandadi2002>
2025-01-11 20:21:57 +0530

    initial commit

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import logo from '../images/Logo.svg'
 
 const Nav = () => {
     return (
         <nav>
-            Nav Page 
+            <a href='/'>
+            <img src={logo} alt='logo'/>
+            </a> 
+
+            <div>
+                <div clssmate='bar'></div>
+                <div clssmate='bar'></div>
+                <div clssmate='bar'></div>
+            </div>
+
+            <ul>
+                <li>
+                <a href="/">Home</a>  
+                </li>
+                <li>
+                <a href="/">About</a>
+                </li>
+                <li>
+                <a href="/">About</a>
+                </li>
+                <li>
+                <a href="/">About</a>
+                 </li>
+                <li>
+                <a href="/">Order Online</a>
+                </li>
+                <li>
+                <a href="/">Login</a>
+                </li>
+            </ul>
         </nav>
     );
 }
