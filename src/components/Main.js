5286e5cf232e5365459170523c752fa6a16fe519
Mandadi2002 <https://github.com/Mandadi2002>
2025-01-14 20:18:04 +0530

    initial commit

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Header from './Header';
+import Booking from './Booking';
+import ConfirmedBooking from './ConfirmedBooking';
 
 const Main = () => {
+    //const {state, dispatch} = useReducer(updateTimes, initialState);  // Assuming useReducer is imported
+    //  The following is a placeholder since useReducer is not fully defined in the provided code.
+    const state = [];
+    const dispatch = () => {};
+    const SubmitForm = () => {}; // Placeholder for SubmitForm
+
     return (
         <main>
-
+            <Routes>
+               <Route path='/' element={<Header/>}/>
+               <Route path='/booking' element={<Booking avaliableTimes={state} dispatch={dispatch} SubmitForm={SubmitForm} />} />
+            </Routes>
         </main>
     );
 };
+
+export default Main;
