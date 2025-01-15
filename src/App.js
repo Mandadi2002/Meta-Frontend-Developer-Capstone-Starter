5c6389cd864c039118564074892056c242476d1a
Mandadi2002 <https://github.com/Mandadi2002>
2025-01-15 10:01:42 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import Nav from './components/Nav';
 import Main from './components/Main';
-import BookingForm from './components/BookingForm';
 
 function App() {
     return (
