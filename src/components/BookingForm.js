da5e7286c21b1647ef4052f215e405947902d6d6
Mandadi2002 <https://github.com/Mandadi2002>
2025-01-15 11:34:50 +0530

    initial commit

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -14,6 +14,10 @@ const BookingForm = (props) => {
         props.SubmitForm(e);
     }
 
+    const handleChange = (e) =>{
+        setDate(e);
+        props.dispatch(e);
+    }
     return (
         <haeder>
             <section>
