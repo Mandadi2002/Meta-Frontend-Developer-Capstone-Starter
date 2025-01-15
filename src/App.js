6496935ccb275116ec03d80c00dea82a462b0d19
Mandadi2002 <https://github.com/Mandadi2002>
2025-01-15 07:45:11 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React from 'react';
 import './App.css';
 import Nav from './components/Nav';
 import Main from './components/Main';
+import BookingForm from './components/BookingForm';
 
 function App() {
     return (
         <>
         <Nav/>
         <Main/>
+        <BookingForm/>
         </>
 
-    )
+    );
 }
 
 export default App;
