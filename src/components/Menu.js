7a93de70833b7ae608e953e09e2aeed0e216f449
Mandadi2002 <https://github.com/Mandadi2002>
2025-01-16 10:45:11 +0530

    initial commit

---
+++ src/components/Menu.js
@@ -0,0 +1,30 @@
+import React from "react";
+import receipes from "../recipes";
+
+
+const Menu = () => {
+    <div classname='menu=container'>
+        <div>
+            <h2>This week specials</h2>
+            <button>Online Menu</button>
+        </div>
+
+        <div>
+            {
+                receipes.map(receipe => <div key={receipe.id}>
+                <img src={receipe.image} alt=''/>
+                <div>
+                    <div>
+                        <h5>{receipe.title}</h5>
+                        <p>{receipe.price}</p>
+                    </div>
+                       <p>{receipe.description}</p>
+                       <button></button>
+                    </div>
+                </div>)
+}
+        </div>
+    </div>
+}
+
+export default Menu;
