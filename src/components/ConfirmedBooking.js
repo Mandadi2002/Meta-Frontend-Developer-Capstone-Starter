c1c4be943eb94a2ae04656ef720ff0b46b3e2219
Mandadi2002 <https://github.com/Mandadi2002>
2025-01-16 08:50:50 +0530

    initial commit

--- src/components/ConfirmedBooking.js
+++ src/components/ConfirmedBooking.js
@@ -5,7 +5,7 @@ const ConfirmedBooking = () => {
     return (
         <div className='confirm'>
          <div>
-            <h1>Booking has been <span>confirmed</span></h1>
+            <h1>Booking has been <span>confirmed!</span></h1>
          </div>
         </div>
     );
