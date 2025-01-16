194760b8850a287ca1e8310577f80114a807ce0b
Mandadi2002 <https://github.com/Mandadi2002>
2025-01-16 15:29:57 +0530

    initial commit

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -31,7 +31,7 @@ const BookingForm = (props) => {
                             <select id='book-time' value={times} onChange={(e) => setTimes(e.target.value)} required>
                                 <option value="">Select a Time</option>
                                 {
-                                    props.avaliableTimes.avaliableTimes.map(avaliableTimes => {return <option key={avaliableTimes}>{avaliableTimes}</option>})
+                                    props.availableTimes.availableTimes.map(availableTimes => {return <option key={availableTimes}>{availableTimes}</option>})
                                 }
                             </select>
                         </div>
