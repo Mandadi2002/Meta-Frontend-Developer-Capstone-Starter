3a5899f2bee4e76460a0b8298c17f82b1e6b54ac
Mandadi2002 <https://github.com/Mandadi2002>
2025-01-16 09:24:38 +0530

    initial commit

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,12 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 
 const Main = () => {
     return (
         <main>
+            <Routes>
+                <Route></Route>
+            </Routes>
 
             </main>
     );
