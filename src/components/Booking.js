2087c1d297e67d89b2f7e98450fc37786a0c689e
Mandadi2002 <https://github.com/Mandadi2002>
2025-01-16 15:32:47 +0530

    initial commit

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -3,7 +3,7 @@ import BookingForm from "./BookingForm";
 
 const Booking = (props) => {
     return (
-         <BookingForm avaliableTimes={props.avaliableTimes} dispatch={props.dispatch} SubmitForm={props.SubmitForm}/> 
+         <BookingForm availableTimes={props.availableTimes} dispatch={props.dispatch} SubmitForm={props.SubmitForm}/> 
     );
 };
 
