ad0180a708df4cfa2f5c132fd16638a51cba16b8
Mandadi2002 <https://github.com/Mandadi2002>
2025-01-16 11:20:45 +0530

    initial commit

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,8 +1,29 @@
 import React from "react";
 import receipes from "../recipes";
-
+import Swal from 'sweetalert2';
 
 const Menu = () => {
+
+    const handleOrder = (id) => {
+        console.log(id,"id is clicked");
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+          }).then((result) => {
+            if (result.isConfirmed) {
+              Swal.fire(
+                'Deleted!',
+                'Your file has been deleted.',
+                'success'
+              )
+            }
+          })
+    }
     return (
     <div classname='menu=container'>
         <div className="menu-header">
